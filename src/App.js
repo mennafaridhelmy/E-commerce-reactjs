42d48e4265848b7cd7f2e5d792eef2a77d5467cc
menna <mennafaridhelmy@gmail.com>
2022-04-14 02:37:25 +0200

    Basic setup and homepage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import Header from './compoents/Header';
+import "./default.scss"
+import Homepage from './pages/Homepage';
 
 function App() {
   return (
     <div className="App">
-      <h2>Hello world!</h2>
+      <Header />
+      <div className='main'>
+        <Homepage />
+      </div>
     </div>
   );
 }
